ce3a136a883554d6029dc10be05a0e090fefe3da
Virgilia Antonucci <virgilia.antonucci@gmail.com>
2022-05-02 17:49:28 -0400

    commit tree dc05dc13a885f7f9f26603db074cce1f26ed411d

--- index.js
+++ index.js
@@ -1,2 +1,42 @@
 
 // Write your code here
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+
+let num1= 2
+let num2 = 31 
+let multiply = num1*num2
+
+function divide(a, b) {
+ return a / b
+}
+
+function increment(n) {
+    return n += 1
+}
+
+function decrement(n) {
+    return n -= 1
+}
+
+function makeInt(string) {
+    return parseInt(string, 10)
+}
+
+function preserveDecimal(string) {
+ return parseFloat(string)
+}
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let num3 = 16
+let num4 = 12
+let mod = num3%num4
+
+let max = Math.max(1, 2, 3, 4, 20)
